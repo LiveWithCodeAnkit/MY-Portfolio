1aa8295cadc1697d8f64f75db5d901fbf411bb8c
LiveWithCodeAnkit <ankityadav119live@gmail.com>
2023-06-21 23:11:08 +0530

    fix Navbar style

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <>
-      <div className="bg-[#1F1E1B] flex justify-around gap-96 p-4 font-Inconsolata font-bold text-white sticky top-0 opacity-90">
+      <div className="bg-[#1F1E1B] z-20 flex justify-around gap-96 p-4 font-Inconsolata font-bold text-white sticky top-0 opacity-90">
         <Image src="/title.png" alt="Img not Found" width={200} height={200} />
         <nav className="flex">
           <ul className="flex items-center justify-around gap-10">
